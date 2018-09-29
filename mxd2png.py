a54a12195ad65400179d11d3179dcb30bf79ac2b
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-09-29 22:41:41 +0300

    initial commit

---
+++ mxd2png.py
@@ -0,0 +1,7 @@
+#import arcpy
+
+#open mxd file
+
+#select records from desired layer with given SQL criteria in given table with ArcMap Standards
+
+#iterate through selected records, take screenshot and save it!
