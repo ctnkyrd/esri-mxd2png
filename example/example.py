34624fbbb37a7d075df435692d7632c84de06bcf
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-09-30 14:20:53 +0300

    remove examples

--- example/example.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '../mxd2jpg')
-import mxd2jpg
-
-
-a = mxd2jpg.mxd2jpg(r'C:\Users\PC1\Desktop\theCode.mxd', 'A:\Balikesir_KAIP\esri-mxd2png\images')
-a.selectLayer("PARSEL_ESKI")
